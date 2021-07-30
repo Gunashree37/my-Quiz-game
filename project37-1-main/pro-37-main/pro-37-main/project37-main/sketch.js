8d8ee0191c3b90572941dd6bc5adb6163aafdf47
Gunashree S <gunashrees341@gmail.com>
2021-07-30 19:19:14 +0530

    Update sketch.js

--- project37-1-main/pro-37-main/pro-37-main/project37-main/sketch.js
+++ project37-1-main/pro-37-main/pro-37-main/project37-main/sketch.js
@@ -10,9 +10,7 @@ var question, contestant, quiz;
 function setup(){
   canvas = createCanvas(850,400);
   database = firebase.database();
-  quiz=new Quiz()
-  quiz.getState()
-  quiz.start()
+ 
   
   
   
@@ -22,12 +20,8 @@ function setup(){
 
 function draw(){
   background("orange");
- if(contestantCount===2){
-   quiz.update(1)
- }
- if(gameState===1){
-   quiz.play()
- } 
+
+  
   
   
   
